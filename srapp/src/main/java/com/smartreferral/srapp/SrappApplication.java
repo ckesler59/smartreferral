fd2ccaf9338273d8f9771934c3652e5a1eeb68fe
ckesler59 <chris-kesler@comcast.net>
2023-12-28 13:19:17 -0700

    initial spring boot app with dependencies

---
+++ srapp/src/main/java/com/smartreferral/srapp/SrappApplication.java
@@ -0,0 +1,13 @@
+package com.smartreferral.srapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrappApplication.class, args);
+    }
+
+}
---
+++ srapp/src/test/java/com/smartreferral/srapp/SrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartreferral.srapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
